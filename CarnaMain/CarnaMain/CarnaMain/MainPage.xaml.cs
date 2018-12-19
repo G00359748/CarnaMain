649965619a06ab825f12acbb7d817eaa73e32c29
SHANE MC CORMACK <G00359748@gmit.ie>
2018-12-19 15:45:55 +0000

    Importing Project - Created mainpage with Image

---
+++ CarnaMain/CarnaMain/CarnaMain/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace CarnaMain
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            addImagesToPages1();
+        }
+
+        private void addImagesToPages1()
+        {
+            var assembly = typeof(MainPage);
+
+            string strfilename = "CarnaMain.Images.Mace-Pier.jpg";
+
+            MacePier.Source = ImageSource.FromResource(strfilename, assembly);
+        }
+
+       
+    }
+}
